2e210c5f902141cf0dfc714b9e3b967cb91952c8
BB20101997 <blessmann-bennet@t-online.de>
2016-04-12 21:14:26 +0200

    added logging

--- src/bb/chat/gui/ChatServerGUI.java
+++ src/bb/chat/gui/ChatServerGUI.java
@@ -7,25 +7,36 @@ import bb.chat.security.BasicUserDatabase;
 import bb.chat.server.ServerChat;
 import bb.net.enums.Side;
 import bb.net.handler.BasicConnectionManager;
+import bb.util.file.log.BBLogHandler;
+import bb.util.file.log.Constants;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.util.logging.Logger;
 
 /**
  * @author BB20101997
  */
 public class ChatServerGUI extends JFrame {
 
-	BasicChat bc;
+	final BasicChat bc;
 
+	@SuppressWarnings("ConstantNamingConvention")
+	private static final Logger log;
+
+	static {
+		log = Logger.getLogger(ChatServerGUI.class.getName());
+		log.addHandler(new BBLogHandler(Constants.getLogFile("ChatServer")));
+	}
+	
 	private class WindowListen extends WindowAdapter {
 		@Override
 		public void windowClosing(WindowEvent e) {
 			super.windowClosing(e);
 			bc.shutdown();
-			System.out.println("Disposing Window");
+			log.fine("Disposing Window");
 			dispose();
 			System.exit(0);
 		}
@@ -34,7 +45,6 @@ public class ChatServerGUI extends JFrame {
 	public ChatServerGUI(int port) {
 
 		super("Server GUI");
-		BasicConnectionManager sch;
 		bc = new ServerChat(new BasicConnectionManager(Side.SERVER, port), new BasicPermissionRegistrie(), new BasicUserDatabase(), new BasicCommandRegistry());
 		BasicChatPanel BCP = new BasicChatPanel(bc);
 		bc.setBasicChatPanel(BCP);
